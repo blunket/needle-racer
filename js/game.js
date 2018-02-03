d829fe02a875654dcfb4cec34a1fa4f0719f7931
Andrew Siegman <blerrnk@gmail.com>
2018-02-03 17:10:36 -0600

    more messages plus arrow key images

--- js/game.js
+++ js/game.js
@@ -9,19 +9,35 @@ function resizeCanvas() {
 	c.height = window.innerHeight;
 }
 
+var story_playing = true;
 story();
 
 function story() {
 	console.log("test");
 	var typed = new Typed('#story-message', {
-		strings: ["Hi there.", "Allow me to introduce myself.", "My name is ...", "My name is Mr. Grumpy ButtonEyes.", "My name is Mr. Grumpy ButtonEyes. ... The third."],
-		typeSpeed: 10,
-		startDelay: 1000,
-		backDelay: 3000,
+		strings: [
+			"Hi there.",
+			"You are looking quite well today,^200 Sir FluffNeedle.^1000",
+			"Allow me to introduce myself.",
+			"My name is Mr. Grumpy ButtonEyes.^250.^250.^250 The third.",
+			"I have a slight problem.",
+			"I've lost one of my beautiful button eyes.",
+			"This is U^100N^100A^100C^100C^100E^100P^100T^100A^100B^100L^100E^100!^250 ABSOLUTELY UNACCEPTABLE!",
+			"GO!^100 GO!^100 AT ONCE!^250 You MUST find it for me!",
+			"I BELIEVE in you,^200 FluffNeedle!!!!!!!!",
+		],
+		typeSpeed: 30,
+		startDelay: 500,
+		backDelay: 1800,
 		loop: false,
 		smartBackspace: true,
 		showCursor: false,
+		onComplete: startGame
 	})
+}
+
+function startGame() {
+	story_playing = false;
 }
 
 function draw() {
